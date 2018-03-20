641fc9a56c94fbaf2e599fb9cc8357ef2d402fce
Shivam Kumar Gupta <shivamgupta01@gmail.com>
2018-03-20 01:00:07 -0700

    Updating Solution

--- java_solution/src/selfDividingNumber728/Solution.java
+++ java_solution/src/selfDividingNumber728/Solution.java
@@ -10,24 +10,21 @@ public class Solution {
 
     public static void main(String[]args){
         Solution solution = new Solution();
-        System.out.println("Solution is : " + solution.selfDividingNumbers(9,11));
+        System.out.println("Solution is : " + solution.selfDividingNumbers(1,22));
     }
     public List<Integer> selfDividingNumbers(int left, int right) {
         int flag = 1;
         List<Integer> listOfSelfDividingNumber = new ArrayList<Integer>();
         for(int i =left;i<=right;i++) {
             flag = 1;
-            String[] parts = (Integer.toString(i).split(""));
 
-            for (String s : parts) {
-                try{
-                    if (Integer.valueOf(i)==0 || Integer.valueOf(i)%Integer.valueOf(s)!=0 ){
+            for (char s : String.valueOf(i).toCharArray()) {
+
+                    if (s=='0' || i%(s-'0')!=0 ){
                         flag = 0;
                         break;
-                    }}
-                catch (Exception e){
-                    flag = 0;
-                }
+                    }
+
 
             }
             if (flag == 1){
