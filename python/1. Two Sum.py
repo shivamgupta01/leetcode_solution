0111f2cb22d1f53d0870f026d33d42a09478fe6c
Shivam Kumar Gupta <shivam@mys-MacBook-Pro.local>
2018-09-06 15:00:03 -0700

    Staging chnages

--- python/1. Two Sum.py
+++ python/1. Two Sum.py
@@ -13,20 +13,16 @@ return [0, 1].
 """
 
 
-
-def twoSums(nums,target):
-           for i in range(len(nums)):
-            for j in (range(len(nums))):
-                #print j
-                if (nums[i] + nums[j] == target):
-                    if i != j:
-                        return i,j
-
-result = twoSums(nums=[3,2,4],target = 6)
-
-print result
+def twoSums(nums, target):
+    for i in range(len(nums)):
+        for j in (range(len(nums))):
+            # print j
+            if (nums[i] + nums[j] == target):
+                if i != j:
+                    return i, j
+            
 
 
+result = twoSums(nums=[3, 2, 4], target=6)
 
-
-
+print(result)
